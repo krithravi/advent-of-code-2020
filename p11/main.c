e69a0e48489a1a57d0e7d5fadcea38a040530c66
krithravi <krithika.x.ravishankar@gmail.com>
2020-12-23 20:57:50 -0600

    update

--- p11/main.c
+++ p11/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 //#define LINE_LENGTH 99
 //#define FILE_SIZE 98
-#define LINE_LENGTH 10
+#define LINE_LENGTH 12
 #define FILE_SIZE 10
 
 #define RED     "\x1b[31m"
@@ -12,16 +12,27 @@
 #define CYAN    "\x1b[36m"
 #define RESET   "\x1b[0m"
 
+int areEqual(char a[FILE_SIZE][LINE_LENGTH - 2], char b[FILE_SIZE][LINE_LENGTH - 2]) {
+	for (int i = 0; i < FILE_SIZE; i++) {
+		for (int j = 0; j < LINE_LENGTH - 2; j++) {
+			if (a[i][j] != b[i][j]) {
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
 int main () {
 	FILE *fp;
 	fp = fopen("smol.txt", "r");
 
 
-	char prev[FILE_SIZE][LINE_LENGTH];
-	char curr[FILE_SIZE][LINE_LENGTH];
+	char prev[FILE_SIZE][LINE_LENGTH - 2];
+	char curr[FILE_SIZE][LINE_LENGTH - 2];
 
-	for (int i = 0; i < FILE_SIZE; i++) {
-		for (int j = 0; j < LINE_LENGTH; j++) {
+	for (int i = 0; i < FILE_SIZE - 2; i++) {
+		for (int j = 0; j < LINE_LENGTH -2; j++) {
 			prev[i][j] = 'a';
 			curr[i][j] = 'a';
 			printf("%c", prev[i][j]);
@@ -34,15 +45,18 @@ int main () {
 	char buf[LINE_LENGTH];
 	while(fgets(buf, sizeof buf, fp) != NULL) {
 
-		char temp[FILE_SIZE];
+		char temp[LINE_LENGTH - 2];
 		int i;
+		printf("%d: ", index);
 		for (i = 0; buf[i] != '\0'; i++) {
-			temp[i] = buf[i];
-			printf("%c", temp[i]);
+			//temp[i] = buf[i];
+			//
+			printf( CYAN "%c" RESET, temp[i]);
 		}
 		//printf("\n");
 		//printf(RED "%s" RESET, buf);
-		//curr[index] = buf;
+		//curr[index] = temp;
+
 		index++;
 	}
 
